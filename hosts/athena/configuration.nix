f9235a7ec6a02319d664694ec3ddb8d509522c60
Erik Hacklin <erik.hacklin@gmail.com>
2025-07-05 22:12:47 +0300

    Add virtualisation.nix to configuration.nix

--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -14,6 +14,7 @@
       ./../../modules/nixos/tailscale.nix
       ./../../modules/nixos/gaming.nix
       ./../../modules/nixos/fonts.nix
+      ./../../modules/nixos/virtualisation.nix
     ];
 
   networking.hostName = "athena"; # Define your hostname.
