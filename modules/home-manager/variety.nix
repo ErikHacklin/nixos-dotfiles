7a29b918da104aba81e6cf82dfd98196f3b789c6
Erik Hacklin <erik.hacklin@gmail.com>
2025-07-05 22:42:12 +0300

    Simplify variety set_wallpaper script

--- modules/home-manager/variety.nix
+++ modules/home-manager/variety.nix
@@ -4,36 +4,6 @@
     # The default is to simply set WP=$1.
     WP=$1
 
-    detect_desktop() {
-        if [ -n "$XDG_CURRENT_DESKTOP" ]; then
-            case "${XDG_CURRENT_DESKTOP,,}" in
-            *"gnome"*) echo "gnome" ;;
-            *"unity"*) echo "unity" ;;
-            *"kde"*) echo "kde" ;;
-            *"xfce"*) echo "xfce" ;;
-            *"lxde"*) echo "lxde" ;;
-            *"lxqt"*) echo "lxqt" ;;
-            *"mate"*) echo "mate" ;;
-            *"cinnamon"*) echo "cinnamon" ;;
-            *"lingmo"*) echo "lingmo" ;;
-            *"deepin"*) echo "deepin" ;;
-            *"trinity"*) echo "trinity" ;;
-            *"fluxbox"*) echo "fluxbox" ;;
-            *"sway"*) echo "sway" ;;
-            *"enlightenment"* | *"moksha"*) echo "enlightenment" ;;
-            *) echo "$XDG_CURRENT_DESKTOP" | tr '[:upper:]' '[:lower:]' ;;
-            esac
-            return
-        fi
-        if [[ "$XDG_SESSION_DESKTOP $DESKTOP_STARTUP_ID" == *"awesome"* ]]; then
-            echo "awesome"
-            return
-        fi
-        echo "unknown"
-    }
-
-    DE=$(detect_desktop)
-
     if [ "$XDG_SESSION_DESKTOP" == "Hyprland" ] || [ "$DESKTOP_SESSION" == "hyprland" ] || [ "$DESKTOP_SESSION" == "river" ]; then
         # Is the swww-daemon running?
         #SWWW=$(pidof swww-daemon)
