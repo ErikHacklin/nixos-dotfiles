dc92856bc8996a17efa5ae2cd5b9a7983e69a477
Erik Hacklin <erik.hacklin@gmail.com>
2025-07-05 22:16:11 +0300

    Fix indentation in firefox.nix

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,16 +1,16 @@
 { pkgs, ... }:
 {
-    programs.firefox = {
-        enable = true;
-				policies = {
-					ExtensionSettings = {
-						# Keeper Password Manager
-						"KeeperFFStoreExtension@KeeperSecurityInc" = {
-							installation_mode = "force_installed";
-							install_url = "https://addons.mozilla.org/firefox/downloads/latest/keeper-password-manager/latest.xpi";
-						};
-					};
-  			};
-    };
-    stylix.targets.firefox.profileNames = [ "default" ];
+	programs.firefox = {
+		enable = true;
+		policies = {
+			ExtensionSettings = {
+				# Keeper Password Manager
+				"KeeperFFStoreExtension@KeeperSecurityInc" = {
+					installation_mode = "force_installed";
+					install_url = "https://addons.mozilla.org/firefox/downloads/latest/keeper-password-manager/latest.xpi";
+				};
+			};
+		};
+	};
+	stylix.targets.firefox.profileNames = [ "default" ];
 }
