4f582af76939dcf627bbea1ccabd6b48c873f1bd
Erik Hacklin <erik.hacklin@gmail.com>
2025-07-07 13:46:58 +0300

    Add lib input and fix syntax issue

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -1,27 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.vscode = {
     enable = true;
-    profiles = {
-      default = {
-        extensions = with pkgs.vscode-extensions; [
-          vscodevim.vim
-          ms-python.python
-          ms-vscode-remote.remote-ssh
-          jnoortheen.nix-ide
-          bierner.color-info
-          github.copilot
-          github.copilot-chat
-
-        ];
-        userSettings = {
-          "git.suggestSmartCommit" = false;
-          "editor.tabSize" = 2;
-          "git.confirmSync" = false;
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "nil";
-          "nix.serverSettings" = {
-            "nil" = {
-              "formatting" = { "command": ["nixfmt"] };
+    mutableExtensionsDir = false;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        ms-python.python
+        ms-vscode-remote.remote-ssh
+        jnoortheen.nix-ide
+        bierner.color-info
+        github.copilot
+        github.copilot-chat
+      ];
+      userSettings = {
+        "git.suggestSmartCommit" = false;
+        "editor.tabSize" = 2;
+        "git.confirmSync" = false;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings" = {
+          "nil" = {
+            "formatting" = {
+              "command" = ["nixfmt"];
             };
           };
         };
