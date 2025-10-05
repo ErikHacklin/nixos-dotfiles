1c52e44e13797bb7f8adbc66d40bd5ffbc18de5b
Erik Hacklin <erik.hacklin@gmail.com>
2025-10-05 17:05:06 +0300

    Additional Git options

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,5 +3,9 @@
     enable = true;
     userName = "Erik Hacklin";
     userEmail = "erik.hacklin@gmail.com";
+    extraConfig = {
+      fetch.prune = true;
+      pull.rebase = false;
+    };
   };
 }
